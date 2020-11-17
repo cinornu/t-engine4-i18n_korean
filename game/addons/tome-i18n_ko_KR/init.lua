db1ca12f245537de81afd50a3674f26d61edf454
cinornu <uueeck@gmail.com>
2020-11-17 22:04:35 +0900

    create addons

--- game/addons/tome-i18n_ko_KR/init.lua
+++ game/addons/tome-i18n_ko_KR/init.lua
@@ -1,11 +1,12 @@
-long_name = "Korean Translation (Temp_v1)"
+long_name = "Korean Translation (Temp)"
 short_name = "i18n_ko_KR"
 for_module = "tome"
 version = {1,7,2}
 weight = 1
-author = { "DarkGod", "darkgod@te4.org" }
+author = { "cinornu", "uueeck@gmail.com" }
 homepage = "http://te4.org/"
-description = [[Korean Translation (Temp_v1) translation.]]
+description = [[Korean Translation (Temp) v1]]
+tags = { 'ui' }
 hooks = true
 data = true
 overload = true
