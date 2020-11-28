d2af81af5b40bbfce8d49c10b90004b0b2a73937
cinornu <uueeck@gmail.com>
2020-11-28 21:16:54 +0900

    fuck steam

--- game/addons/tome-i18n_ko_KR/init.lua
+++ game/addons/tome-i18n_ko_KR/init.lua
@@ -1,12 +1,12 @@
-long_name = "Korean Translation (Temp)"
+long_name = "Kor Addons for ToME4 1.7 (TEST)"
 short_name = "i18n_ko_KR"
 for_module = "tome"
 version = {1,7,2}
-addon_version = { 1, 0, 2 }
+addon_version = { 1, 1, 0 }
 weight = 1
 author = { "cinornu", "uueeck@gmail.com" }
 homepage = "http://te4.org/"
-description = [[Korean Translation (Temp) v1]]
+description = [[Korean Translation TEST VERSION!]]
 tags = { 'ui' }
 hooks = true
 data = true
