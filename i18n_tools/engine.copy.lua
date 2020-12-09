dfc80532d1c1cbb3b6507a83cfcea3d6d6fdfd50
cinornu <uueeck@gmail.com>
2020-12-09 15:01:12 +0900

    emergency fix!

--- i18n_tools/engine.copy.lua
+++ i18n_tools/engine.copy.lua
@@ -81,17 +81,17 @@ setFlag("noun_target_sub", function(str, type, noun)
 		return str:gsub("#Target#", noun):gsub("#Target1#", addJosa(noun, "가")):gsub("#Target2#", addJosa(noun, "는")):gsub("#Target3#", addJosa(noun, "를")):gsub("#Target4#", addJosa(noun, "로")):gsub("#Target5#", addJosa(noun, "다")):gsub("#Target6#", addJosa(noun, "과")):gsub("#Target7#", addJosa(noun, 7))
 	elseif type == "#target#" then
 		return str:gsub("#target#", noun):gsub("#target1#", addJosa(noun, "가")):gsub("#target2#", addJosa(noun, "는")):gsub("#target3#", addJosa(noun, "를")):gsub("#target4#", addJosa(noun, "로")):gsub("#target5#", addJosa(noun, "다")):gsub("#target6#", addJosa(noun, "과")):gsub("#target7#", addJosa(noun, 7))
-	elseif str == "@Source@" then
+	elseif type == "@Source@" then
 		return str:gsub("@Source@", noun):gsub("@Source1@", addJosa(noun, "가")):gsub("@Source2@", addJosa(noun, "는")):gsub("@Source3@", addJosa(noun, "를")):gsub("@Source4@", addJosa(noun, "로")):gsub("@Source5@", addJosa(noun, "다")):gsub("@Source6@", addJosa(noun, "과")):gsub("@Source7@", addJosa(noun, 7))
-	elseif str == "@source@" then
+	elseif type == "@source@" then
 		return str:gsub("@source@", noun):gsub("@source1@", addJosa(noun, "가")):gsub("@source2@", addJosa(noun, "는")):gsub("@source3@", addJosa(noun, "를")):gsub("@source4@", addJosa(noun, "로")):gsub("@source5@", addJosa(noun, "다")):gsub("@source6@", addJosa(noun, "과")):gsub("@source7@", addJosa(noun, 7))
-	elseif str == "@Target@" then
+	elseif type == "@Target@" then
 		return str:gsub("@Target@", noun):gsub("@Target1@", addJosa(noun, "가")):gsub("@Target2@", addJosa(noun, "는")):gsub("@Target3@", addJosa(noun, "를")):gsub("@Target4@", addJosa(noun, "로")):gsub("@Target5@", addJosa(noun, "다")):gsub("@Target6@", addJosa(noun, "과")):gsub("@Target7@", addJosa(noun, 7))
-	elseif str == "@target@" then
+	elseif type == "@target@" then
 		return str:gsub("@target@", noun):gsub("@target1@", addJosa(noun, "가")):gsub("@target2@", addJosa(noun, "는")):gsub("@target3@", addJosa(noun, "를")):gsub("@target4@", addJosa(noun, "로")):gsub("@target5@", addJosa(noun, "다")):gsub("@target6@", addJosa(noun, "과")):gsub("@target7@", addJosa(noun, 7))
 	elseif str == "@playername@" then
 		return str:gsub("@playername@", noun):gsub("@playername1@", addJosa(noun, "가")):gsub("@playername2@", addJosa(noun, "는")):gsub("@playername3@", addJosa(noun, "를")):gsub("@playername4@", addJosa(noun, "로")):gsub("@playername5@", addJosa(noun, "다")):gsub("@playername6@", addJosa(noun, "과")):gsub("@playername7@", addJosa(noun, 7))
-	elseif str == "@npcname@" then
+	elseif type == "@npcname@" then
 		return str:gsub("@npcname@", noun):gsub("@npcname1@", addJosa(noun, "가")):gsub("@npcname2@", addJosa(noun, "는")):gsub("@npcname3@", addJosa(noun, "를")):gsub("@npcname4@", addJosa(noun, "로")):gsub("@npcname5@", addJosa(noun, "다")):gsub("@npcname6@", addJosa(noun, "과")):gsub("@npcname7@", addJosa(noun, 7))
 	else
 		return str:gsub(type, noun)
